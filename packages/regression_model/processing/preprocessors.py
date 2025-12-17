775a46ee570fc5f47d59864a39cac237c2713f50
Bouzit Thala <bouzit.thala@gmail.com>
2025-12-16 21:29:21 -0500

    Partie-3-Ajout predictions et tests

--- packages/regression_model/processing/preprocessors.py
+++ packages/regression_model/processing/preprocessors.py
@@ -89,7 +89,7 @@ class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
 
         for var in self.variables:
             # the encoder will learn the most frequent categories
-            t = pd.Series(X[var].value_counts() / float(len(X)))
+            t = pd.Series(X[var].value_counts() / np.float(len(X)))
             # frequent labels:
             self.encoder_dict_[var] = list(t[t >= self.tol].index)
 
