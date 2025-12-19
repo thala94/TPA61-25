09a73bec99b22f8f032759b64668340dbbb87c97
Bouzit Thala <bouzit.thala@gmail.com>
2025-12-18 21:11:49 -0500

    Partie-5-Ajout ingenierie des fonctionnalites dans le pipeline

--- packages/regression_model/regression_model/processing/preprocessors.py
+++ packages/regression_model/regression_model/processing/preprocessors.py
@@ -89,7 +89,7 @@ class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
 
         for var in self.variables:
             # the encoder will learn the most frequent categories
-            t = pd.Series(X[var].value_counts() / float(len(X)))
+            t = pd.Series(X[var].value_counts() / np.float(len(X)))
             # frequent labels:
             self.encoder_dict_[var] = list(t[t >= self.tol].index)
 
@@ -143,36 +143,6 @@ class CategoricalEncoder(BaseEstimator, TransformerMixin):
                 f"Categorical encoder has introduced NaN when "
                 f"transforming categorical variables: {vars_.keys()}"
             )
-
-        return X
-
-
-class LogTransformer(BaseEstimator, TransformerMixin):
-    """Logarithm transformer."""
-
-    def __init__(self, variables=None):
-        if not isinstance(variables, list):
-            self.variables = [variables]
-        else:
-            self.variables = variables
-
-    def fit(self, X, y=None):
-        # to accomodate the pipeline
-        return self
-
-    def transform(self, X):
-        X = X.copy()
-
-        # check that the values are non-negative for log transform
-        if not (X[self.variables] > 0).all().all():
-            vars_ = self.variables[(X[self.variables] <= 0).any()]
-            raise ValueError(
-                f"Variables contain zero or negative values, "
-                f"can't apply log for vars: {vars_}"
-            )
-
-        for feature in self.variables:
-            X[feature] = np.log(X[feature])
 
         return X
 
